1d3fb9a7ca85327736a36bd37d69d9988bdf1ae0
Rob <rob:@jarratt.me.uk>
2016-12-24 18:41:05 +0100

    Added some memory access basics by creating a SAC module

---
+++ MU5/mu5_sac.c
@@ -0,0 +1,51 @@
+/* mu5_sac.c: MU5 Store Access Control unit
+
+Copyright (c) 2016-2017, Robert Jarratt
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ROBERT M SUPNIK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of Robert Jarratt shall not be
+used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from Robert Jarratt.
+*/
+
+#include "mu5_defs.h"
+#include "mu5_sac.h"
+
+static uint32 LocalStore[MAXMEMORY];
+
+uint16 sac_read_16_bit_word(uint16 address)
+{
+	uint32 fullWord = LocalStore[address > 1];
+	uint16 result = (address & 1) ? fullWord >> 16 : fullWord & 0xFFFF;
+	return result;
+}
+
+void sac_write_16_bit_word(uint16 address, uint16 value)
+{
+	uint32 fullWord = LocalStore[address > 1];
+	if (address & 1)
+	{
+		fullWord = (value < 16) | (fullWord & 0xFFFF);
+	}
+	else
+	{
+		fullWord = (fullWord & 0xFFFF0000) | value;
+	}
+	LocalStore[address > 1] = fullWord;
+}
