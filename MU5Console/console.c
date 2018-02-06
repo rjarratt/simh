85e13d884b83082fc97ef121a7704fb9ae378c3c
Rob <rob:@jarratt.me.uk>
2018-02-06 21:58:21 +0000

    Corrected lamp text for PROP STOPPER

--- MU5Console/console.c
+++ MU5Console/console.c
@@ -452,8 +452,9 @@ static void DrawLampPanelLampOverlay(void)
 	DrawLampTextVertical(0, 32, "1905E", 0);
 	DrawLampTextVertical(0, 33, "MASS", 0);
 	DrawLampTextVertical(0, 34, "LOCAL", 0);
-	DrawLampTextVertical(0, 38, "IBU", 0);
-	DrawLampTextVertical(0, 39, "VALID+", 1);
+    DrawLampTextVertical(0, 37, "STOPPER", 2);
+    DrawLampTextVertical(0, 38, "IBU", 0);
+    DrawLampTextVertical(0, 39, "VALID+", 1);
 
 	/* row 2 */
 	DrawLampRegisterNibbleLabelDivider(1, 0, 16);
